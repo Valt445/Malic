8df69067e9cf06bfc16d691236d97db5237ca908
Valker <mananb239@gmail.com>
2024-04-09 21:16:05 +0530

    just doing a test

--- Malic/src/Malic/EnteryPoint.h
+++ Malic/src/Malic/EnteryPoint.h
@@ -8,7 +8,7 @@ int main(int argc, char** argv)
 {
 	Malic::Log::Init();
 	MA_CORE_WARN("hallo neice and nephew");
-	MA_TRACE("fuiyoh uncle gordan make good fried rice");
+	MA_TRACE("haiyaa uncle gordan make good fried rice");
 
 	auto app = Malic::CreateApplication();
 	app->run();

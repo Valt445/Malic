ebeb73f73ab6a6ac6add4be28cc397a5d9ae1296
Valker <mananb239@gmail.com>
2024-04-10 13:14:55 +0530

    added a premake build system

--- Malic/src/Malic/EnteryPoint.h
+++ Malic/src/Malic/EnteryPoint.h
@@ -9,6 +9,8 @@ int main(int argc, char** argv)
 	Malic::Log::Init();
 	MA_CORE_WARN("hallo neice and nephew");
 	MA_TRACE("fuiyoh uncle gordan make good fried rice");
+	MA_CORE_INFO("HELLO");
+
 
 	auto app = Malic::CreateApplication();
 	app->run();
---
+++ premake5.lua
@@ -0,0 +1,111 @@
+workspace "Malic"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Malic"
+	location "Malic"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Malic/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"MA_BUILD_DLL",
+			"MA_PLATFORM_WINDOWS"
+
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "MA_DEBUG"
+		symbols "On"
+
+	
+	filter "configurations:Release"
+		defines "MA_RELEASE"
+		optimize "On"
+
+	
+	filter "configurations:Dist"
+		defines "MA_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Malic/vendor/spdlog/include",
+		"Malic/src"
+	}
+
+	links
+	{
+		"Malic"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"MA_PLATFORM_WINDOWS"
+
+		}
+
+	filter "configurations:Debug"
+		defines "MA_DEBUG"
+		symbols "On"
+
+	
+	filter "configurations:Release"
+		defines "MA_RELEASE"
+		optimize "On"
+
+	
+	filter "configurations:Dist"
+		defines "MA_DIST"
+		optimize "On"
